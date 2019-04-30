e497113b34b20ea57505076d572a7a57a2f6c2d7
Tim timmyRS Speckhals <github@timmyrs.de>
2019-04-30 21:07:23 +0200

    Add test for Base64Cipher

--- tests/CipherTest.php
+++ tests/CipherTest.php
@@ -1,9 +1,14 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
-use cipherfinder\HexCipher;
+use cipherfinder\{Base64Cipher, HexCipher};
 use PHPUnit\Framework\TestCase;
 final class CipherTest extends TestCase
 {
+	function testBase64Cipher()
+	{
+		$this->assertEquals("Hello, world!", (new Base64Cipher())->decode("SGVsbG8sIHdvcmxkIQ=="));
+	}
+
 	function testHexCipher()
 	{
 		$this->assertEquals("\x0F", (new HexCipher())->decode("0f"));
